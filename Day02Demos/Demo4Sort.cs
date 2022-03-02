ffe1bbf564817d77ad55d8b30310a11d1789a74f
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-03-02 16:53:06 +0530

    Added Assignment

---
+++ Day02Demos/Demo4Sort.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Demo4Sort
+{
+    public static void Test()
+    {
+        Console.WriteLine("======= Sort Demo =======");
+        Console.Write("How many numbers do you want to sort: ");
+        string totalText = Console.ReadLine();
+        int total = int.Parse(totalText);
+
+        int[] arr = new int[total];
+
+        for (int i = 0; i < total; i++)
+        {
+            Console.Write("Enter number " + (i + 1) + ": ");
+            string numText = Console.ReadLine();
+            arr[i] = int.Parse(numText);
+        }
+
+        Console.WriteLine("\nThe unsorted array is: ");
+        Display(arr);
+        
+        Sort(arr);
+        
+        Console.WriteLine("\n\nThe sorted array is: ");
+        Display(arr);
+    }
+
+    public static void Sort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }    
+            }
+        }
+    }
+
+    static void Display(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + ",");
+        }
+    }
+}
+
+
--- Day02Demos/Program.cs
+++ Day02Demos/Program.cs
@@ -4,4 +4,50 @@ Console.WriteLine("Hello, World!");
 
 //Demo1CommandLineArguments.Test(args);
 //Demo2Palindrome.Test();
-Demo3LinearSearch.Test();
+//Demo3LinearSearch.Test();
+//Demo4Sort.Test();
+
+Demo5BinarySearch.Test();
+
+class Demo5BinarySearch
+{
+    public static void Test()
+    {
+        int[] arr = new[] { 5, 7, 9, 3, 2, 1, 55, 44, 88, 66, 33, 22 };
+
+        Demo4Sort.Sort(arr);
+
+        do
+        {
+            Console.Write("Enter number to search: ");
+            string numText = Console.ReadLine();
+
+            if (numText == "0")
+                return;
+
+            if (BinarySearch(arr, int.Parse(numText)))
+                Console.WriteLine("Found Successfully");
+            else
+                Console.WriteLine("Did not find this number");
+        }while(true);
+    }
+
+    private static bool BinarySearch(int[] arr, int num)
+    {
+        int lowIndex = 0, highIndex = arr.Length - 1;
+
+        while (lowIndex <= highIndex)
+        {
+            int midIndex = (highIndex + lowIndex) / 2;
+
+            if (num == arr[midIndex])
+                return true;
+            else if (num < arr[midIndex])
+                highIndex = midIndex - 1;
+            else if (num > arr[midIndex])
+                lowIndex = midIndex + 1;
+        }
+
+        return false;
+    }
+}
